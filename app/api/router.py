2ba93a65955597088e593e963ce4ea114aa88d05
Sergio Hervás Cobo <sergiohervas18@gmail.com>
2025-02-05 18:40:42 +0100

    Authorization fixed. The prefix v1 was duplicated.

--- app/api/router.py
+++ app/api/router.py
@@ -3,4 +3,4 @@ from fastapi import APIRouter
 from .api_v1.router import api_router as api_v1_router
 
 api_router = APIRouter()
-api_router.include_router(api_v1_router, prefix="/v1")
+api_router.include_router(api_v1_router)
